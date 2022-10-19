c85eba63e9d5b148965b7a0d89dfa11793e7836e
Ben Collerson <benc@benc.cc>
2022-10-19 21:30:58 +1000

    some app rules

--- config.h
+++ config.h
@@ -43,10 +43,11 @@ static const Rule rules[] = {
 /* xprop(1):
  *        WM_CLASS(STRING) = instance, class
  *        WM_NAME(STRING) = title
-	   class          instance  title            tags mask  isfloating  monitor */
-	{ "KeePassXC",    NULL,     NULL,            0,         1,          -1 },
-	{ "st-256color",  NULL,     NULL,            0,         0,          -1 },
-	{ NULL,           NULL,     "Event Tester",  0,         0,          -1 },
+	   class          instance  title            tags mask       isfloating  monitor */
+	{ "KeePassXC",    NULL,     NULL,            (1 << 31),      1,          -1 },
+	{ "qutebrowser",  NULL,     "YouTube",       1 << ('y'-'a'), 0,          -1 },
+	{ "st-256color",  NULL,     NULL,            0,              0,          -1 },
+	{ NULL,           NULL,     "Event Tester",  0,              0,          -1 },
 };
 
 /* layout(s) */
--- dwm.c
+++ dwm.c
@@ -354,7 +354,8 @@ applyrules(Client *c)
 		XFree(ch.res_class);
 	if (ch.res_name)
 		XFree(ch.res_name);
-	c->tags = c->tags & TAGMASK ? c->tags & TAGMASK : c->mon->tagset[c->mon->seltags];
+	/*c->tags = c->tags & TAGMASK ? c->tags & TAGMASK : c->mon->tagset[c->mon->seltags];*/
+	c->tags = c->tags ? c->tags : c->mon->tagset[c->mon->seltags];
 }
 
 int
